c982a060bcbf56b7887d8ea96554a52e60be913b
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-08-16 23:43:38 +0530

    uncommented

--- App.js
+++ App.js
@@ -7,56 +7,56 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 // import { useDimensions, useDeviceOrientationa } from 'react-native-community/hooks';
 
 export default function App() {
-  // console.log("App Executed");
-  // console.log(Dimensions.get("screen"));
-  // const handlePress = () => console.log("Text Pressed");
-  // // const {landscape} = useDeviceOrientationa()
+  console.log("App Executed");
+  console.log(Dimensions.get("screen"));
+  const handlePress = () => console.log("Text Pressed");
+  // const {landscape} = useDeviceOrientationa()
  
-  // return (
-  //   <SafeAreaView style={styles.container}>
-  //     <Text numberOfLines={2} style = {styles.paragraph} onPress={ handlePress }>
-  //          Change code in the editor and watch it change on your phone! Save to get a shareable react url.
-  //     </Text>
-  //     <TouchableNativeFeedback onPress={() => console.log("Image Touched")}>
-  //     <Image 
-  //       // blurRadius={10}
-  //       fadeDuration={1000}   
-  //       source={{
-  //         width: 200, 
-  //         height: 300, 
-  //         uri: "https://picsum.photos/200/300"
-  //       }}
-  //     />
-  //     </TouchableNativeFeedback>
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2} style = {styles.paragraph} onPress={ handlePress }>
+           Change code in the editor and watch it change on your phone! Save to get a shareable react url.
+      </Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image Touched")}>
+      <Image 
+        // blurRadius={10}
+        fadeDuration={1000}   
+        source={{
+          width: 200, 
+          height: 300, 
+          uri: "https://picsum.photos/200/300"
+        }}
+      />
+      </TouchableNativeFeedback>
       
      
-  //     {/* <StatusBar style="auto" /> */}
+      {/* <StatusBar style="auto" /> */}
 
-  //     <View style ={[containerStyle ]}>
-  //      <Button 
-  //       color="orange" 
-  //       title="Click me" 
-  //       onPress={() => 
-  //         Alert.alert("My Title", "My Message", [
-  //           {text: "Yes", onPress: () => console.log("Yes Pressed")}, 
-  //           {text: "No", onPress: () => console.log("No Pressed")},
-  //         ])}/>
-  //     </View>
-  //   </SafeAreaView>
+      <View style ={[containerStyle ]}>
+       <Button 
+        color="orange" 
+        title="Click me" 
+        onPress={() => 
+          Alert.alert("My Title", "My Message", [
+            {text: "Yes", onPress: () => console.log("Yes Pressed")}, 
+            {text: "No", onPress: () => console.log("No Pressed")},
+          ])}/>
+      </View>
+    </SafeAreaView>
 
     
 
-  //   // <SafeAreaView style = {styles.container}>
-  //   //   <View style = {{
-  //   //     backgroundColor:"orange",
-  //   //     width:"100%",
-  //   //     height: landscape? "100%" : "30%"
-  //   //   }}>
+    // <SafeAreaView style = {styles.container}>
+    //   <View style = {{
+    //     backgroundColor:"orange",
+    //     width:"100%",
+    //     height: landscape? "100%" : "30%"
+    //   }}>
 
-  //   //   </View>
+    //   </View>
       
-  //   // </SafeAreaView>
-  // );
+    // </SafeAreaView>
+  );
   
   return <ViewImageScreen/>
 }
